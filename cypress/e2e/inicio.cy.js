938b94fc2291f9a926a2bf467ebda85765c0bf2d
Bruno Tenório <tenoriobn@gmail.com>
2024-06-10 18:11:41 -0300

    Finalizando Desafio de criar primeiro teste com cypress

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,5 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Deve renderizar corretamente o texto da seção de vantagens', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('h2').contains('Vantagens do nosso banco')
   });
 })
