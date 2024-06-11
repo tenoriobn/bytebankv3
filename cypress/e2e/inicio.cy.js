126194662bcdf076af7f58bc572d5af8a25a9e10
Bruno Tenório <tenoriobn@gmail.com>
2024-06-11 18:52:25 -0300

    Depurando o erro

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/');
+    cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   });
 
   it('Deve renderizar corretamente o texto da seção de vantagens', () => {
