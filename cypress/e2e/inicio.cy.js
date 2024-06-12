7931a6ed9ae53f95f6e6a724941627d2c3bab9a9
Bruno Tenório <tenoriobn@gmail.com>
2024-06-12 19:20:56 -0300

    Utilizando data atributos no teste

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,7 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Página Inicial', () => {
+  it('Deve renderizar o h1 com o texto correto', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   });
 
   it('Deve renderizar corretamente o texto da seção de vantagens', () => {
