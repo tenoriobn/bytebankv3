882d0ba68fb647aa3ab84d49f2a64ec033104b35
Bruno Tenório <tenoriobn@gmail.com>
2024-06-17 18:08:10 -0300

    Criando comandos personalizados

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,11 +1,13 @@
 describe('Página Inicial', () => {
-  it('Deve renderizar o h1 com o texto correto', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/');
-    cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+  });
+  
+  it('Deve renderizar o h1 com o texto correto', () => {
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   });
 
   it('Deve renderizar corretamente o texto da seção de vantagens', () => {
-    cy.visit('http://localhost:3000/');
     cy.get('h2').contains('Vantagens do nosso banco')
   });
 })
