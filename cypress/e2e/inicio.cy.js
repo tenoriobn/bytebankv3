7ebb1885323b0d0c05bf4f7d0077b5381dec4cf9
Bruno Tenório <tenoriobn@gmail.com>
2024-06-18 18:25:46 -0300

    refatorando teste que confere titulo das vantagens

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -4,12 +4,10 @@ describe('Página Inicial', () => {
   });
 
   it('Deve renderizar o h1 com o texto correto', () => {
-    // cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
-
     cy.getByText('titulo-principal', 'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!');
   });
 
   it('Deve renderizar corretamente o texto da seção de vantagens', () => {
-    cy.get('h2').contains('Vantagens do nosso banco')
+    cy.getByText('titulo-vantagens', 'Vantagens do nosso banco');
   });
 })
--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -22,7 +22,7 @@ export default function Inicio() {
             />
           </div>
           <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
+            <h2 data-test="titulo-vantagens">Vantagens do nosso banco:</h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
                 <img src={presente} alt="ícone de um presente" />
