a51096532fd73f24287e56f487e6a7b16ddfae45
Bruno Tenório <tenoriobn@gmail.com>
2024-06-19 21:00:36 -0300

    realizando test de login

---
+++ cypress/e2e/formularioLogin.cy.js
@@ -0,0 +1,13 @@
+describe('Formulario de Login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Não deve permitir um email inválido', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type('neilton@alura');
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido');
+  })
+})
