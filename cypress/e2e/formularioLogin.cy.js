32b4d161329184a9519fb5337df39d002c24285f
Bruno Tenório <tenoriobn@gmail.com>
2024-06-21 11:24:41 -0300

    Enviando formulário vazio

--- cypress/e2e/formularioLogin.cy.js
+++ cypress/e2e/formularioLogin.cy.js
@@ -9,5 +9,13 @@ describe('Formulario de Login', () => {
     cy.getByData('senha-input').type('123456');
     cy.getByData('botao-enviar').click();
     cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido');
-  })
+  });
+
+  it.only('Não deve permitir um campo em branco', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório');
+  });
+  
 })
