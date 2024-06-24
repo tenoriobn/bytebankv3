0b8a3afb2f123f41224b3b33cafb27fb9124b4db
Bruno Tenório <tenoriobn@gmail.com>
2024-06-24 17:10:18 -0300

    Configurando o baseUrl

--- cypress/cypress/e2e/formularioCadastro.cy.js
+++ cypress/cypress/e2e/formularioCadastro.cy.js
@@ -11,4 +11,12 @@ describe('Formulário Cadastro', () => {
     cy.getByData('botao-enviar').click();
     cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!');
   });  
+
+  it.only('Não deve permitir o cadastro de usuários sem o campo nome preenchido', ()=>{
+    cy.getByData('botao-cadastro').click()
+    cy.getByData('email-input').type('moni@alura.com')
+    cy.getByData('senha-input').type('987654')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de nome é obrigatório')
+  }) 
 });
