e174793271ed8e2b8eaa4c399a8b8e8bfdfb615a
Bruno Tenório <tenoriobn@gmail.com>
2024-08-12 10:02:26 -0300

    testando url e path

--- cypress/cypress/e2e/paginas.cy.js
+++ cypress/cypress/e2e/paginas.cy.js
@@ -1,6 +1,13 @@
 describe('Testando múltiplas páginas', () => {
   it('Deve conseguir acessar a página de cartões', () => {
     cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+
+    cy.getByData('app-home').find('a').eq(1).click()
+    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
   });
   
 })
