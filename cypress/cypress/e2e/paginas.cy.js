8dfd008865df32d09e5866b0c8e4e887390f495b
Bruno Tenório <tenoriobn@gmail.com>
2024-08-13 10:42:03 -0300

    Entendendo a jornada do usuário

--- cypress/cypress/e2e/paginas.cy.js
+++ cypress/cypress/e2e/paginas.cy.js
@@ -6,8 +6,12 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
 
+    cy.location('pathname').should('eq', '/home')
+
     cy.getByData('app-home').find('a').eq(1).click()
     cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+
+    cy.location('pathname').should('eq', '/home/cartoes')
   });
   
 })
